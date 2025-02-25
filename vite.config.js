7baeec0993b564546c4cfd2a3dc302bf3895fe92
Maciej Fiałkowski <Fialek85@gmail.com>
2025-02-25 20:54:55 +0100

    chore(config): add base config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/scss/main.scss", "resources/js/app.js"],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, "node_modules"),
+        },
+    },
+    css: {
+        postcss: "./postcss.config.js",
+    },
 });
